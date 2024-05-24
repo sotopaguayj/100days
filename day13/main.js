c8391a1477f1710045d9049d6fa9c97b41249e42
sotopaguayj <sotopaguayj@gmail.com>
2024-05-23 22:51:57 -0500

    add day 13

---
+++ day13/main.js
@@ -0,0 +1,11 @@
+const starts = document.querySelectorAll('.starts')
+
+starts.forEach((start) => {
+  start.innerHTML = `
+    <img src="./images/icon-star.svg" alt="starts">
+    <img src="./images/icon-star.svg" alt="starts">
+    <img src="./images/icon-star.svg" alt="starts">
+    <img src="./images/icon-star.svg" alt="starts">
+    <img src="./images/icon-star.svg" alt="starts">
+    `
+})
